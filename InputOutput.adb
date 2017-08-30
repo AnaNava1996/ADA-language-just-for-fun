7282adb8cab7d8e55b364eac3239d33c58dc8d32
Ana Paola Nava Vivas <ananava1996@gmail.com>
2017-08-30 02:11:50 -0500

    Add files via upload
    Put a name of 3 letters like Ana :) otherwise you have yo change the 3 in the string to another number

---
+++ InputOutput.adb
@@ -0,0 +1,11 @@
+with Text_IO; use Text_IO;
+procedure InputOutput is
+
+MyName: String (1 .. 3);
+
+begin
+   Put_Line("What's your name?: ");
+   Get(MyName);
+   Put_Line("Hello: " &MyName);
+
+end InputOutput;
